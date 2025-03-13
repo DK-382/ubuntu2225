e0fc32695647fc0f0e8dc03c6de0906fe1a88764
tmxk2018 <19825167213@139.com>
2025-03-13 15:07:06 +0800

    over

---
+++ ubuntu22/ubuntu22-main/Arcs/视觉与通信案例/3-给出相对偏移点VS变量点正逆解/Line_Joint.lua
@@ -0,0 +1,126 @@
+    --自定义函数区
+    --分割字符串函数输出列表
+function string.split(str,delimiter)
+	if str  ==  nil or str ==  ''  or  delimiter  == nil then
+   return  nil
+	end
+	local  result =  {}
+	for match in  (str..delimiter):gmatch("(.-)"..delimiter) do
+   table.insert(result,match)
+	end
+	return result
+end
+    pi = 3.1416
+    --弧度转换函数单个
+    function toRad(degs)
+        if degs==nil or degs=="" then
+            return nil
+        end
+        return degs*(pi/180) 
+    end
+    -- 弧度转角度单个
+    function toDeg(rads)
+        if rads==nil or rads =="" then
+            return nil
+        end
+        return rads*(180/pi)
+    end
+    
+    -- 将角度列表转换为弧度列表
+    function degToRadList(degrees)
+        if degrees==nil or degrees=="" then
+            return nil
+        end
+        local radians = {}
+        for i , j in ipairs(degrees) do
+            radians[i] =j*(pi/180)
+        end
+        return radians
+    end
+    
+    -- 将弧度列表转换为角度列表
+    function radToDegList(radians)
+        if radians==nil or radians=="" then
+            return nil
+        end
+        local degrees = {}
+        for a , b in ipairs(radians) do
+            degrees[a] =b*(180/pi)
+        end
+        return degrees
+    end
+
+
+
+----------------------------------------------------------
+function addLists(list1, list2)
+    -- 检查两个列表长度是否相等，如果不相等返回nil
+    if #list1 ~= #list2 then
+        return nil
+    end
+
+    -- 创建一个新的列表来存储结果
+    local result = {}
+
+    -- 使用for循环遍历列表，进行对应元素相加
+    for i = 1, #list1 do
+        result[i] = list1[i] + list2[i]
+    end
+
+    -- 返回结果列表
+    return result
+end
+
+----------------------------------------------------------
+-- 变量清理
+currentJointA={0}
+currentJointDeg={0}
+currentPosLine={0}
+currentPoseOK={0}
+
+dz=0.1
+
+-- Get Current Point
+currentPosLine=getTcpPose()
+-- currentPosSend={currentPosLine[1]*1000,currentPosLine[2]*1000,currentPosLine[3]*1000,toDeg(currentPosLine[4]),toDeg(currentPosLine[5]),toDeg(currentPosLine[6])}
+
+cpup=currentPosLine
+cpup[3]=cpup[3]+dz
+
+currentJointRad = getJointPositions()-- return rad
+currentJointDeg=radToDegList(currentJointRad)
+
+-- 自定义点位
+P1JointDeg={-24.24,-11.48,-122.25,-20.81,-90.01,-48.94}
+P1JointRad=degToRadList(P1JointDeg)
+
+P1PosOri={-389.93,42.32,294.59,toRad(179.966),toRad(0.0573),toRad(-65.317)}
+P1Pos={P1PosOri[1]/1000,P1PosOri[2]/1000,P1PosOri[3]/1000,P1PosOri[4],P1PosOri[5],P1PosOri[6]}
+
+-- P2Pos=P1Pos
+-- P2Pos[3]=P1Pos[3]+0.1
+
+manualOffset={0.1,0.1,0.2,0,0,0}
+P2Pos=addLists(P1Pos,manualOffset)
+
+---------------------------主控部分--------------------------------
+--机械臂移动控制区
+
+-- moveJoint(currentJointRad, 1.0, 1.0, 0, 0)
+-- moveJoint(P1JointRad, 1.0, 1.0, 0, 0)
+
+-- moveLine(currentPosLine,1,1,0,0)
+-- moveLine(cpup,1,1,0,0)
+moveLine(P1Pos,1,1,0,0)
+moveLine(P2Pos,1,1,0,0)
+
+setTcpOffset({0,0,0,0,0,0})
+
+moveJoint(inverseKinematics(currentJointRad, P1Pos), 1.0, 1.0, 0, 0)
+
+CPJointRad=getJointPositions()
+CPPose=getTcpPose()
+pzjPos=forwardKinematics(degToRadList(P1JointRad))
+pnjPos=inverseKinematics(currentJointRad, P1Pos)
+
+textmsg("script running finish normally")
